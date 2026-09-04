dd233963307ed0bf6eb856d833e270b4b828c14f
superbug098 <superbug098@gmail.com>
2026-09-04 12:38:40 +0800

    Update hd2312-fe.c

--- hd2312-fe.c
+++ hd2312-fe.c
@@ -216,9 +216,9 @@ static int hd2312_read_status(struct dvb_frontend *fe, enum fe_status *status)
 	ret = usb_control_msg(dev, usb_rcvctrlpipe(dev, 0),
 		CYUSB_HD2312_GET_STRENGTH,
 		USB_TYPE_VENDOR | USB_DIR_IN,
-		0xFE, 0, buf, 2, 500);
+		0xFE, 0, buf, 4, 500);
 
-	if (ret == 2) {
+	if (ret == 4) {
 		pr_debug("%s: strength:%02X , %02X\n", __func__, buf[0], buf[1]);
 
 		c->strength.stat[0].scale =FE_SCALE_DECIBEL;
